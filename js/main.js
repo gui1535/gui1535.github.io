b087d6912de3726a7c292346d04f5f370e7672fb
gui1535 <guilherme.araujo1535@gmail.com>
2022-05-31 17:33:21 -0300

    Refatoração total no codigo

--- js/main.js
+++ js/main.js
@@ -1,102 +1,26 @@
-$(function () {
-    var filterList = {
-        init: function () {
-            $('.portfolio-grid').mixItUp({
-                selectors: {
-                    target: '.portfolio',
-                    filter: '.filter'
-                },
-                load: {
-                    filter: 'all' // show app tab on first load
-                }
+// MENU MOBILE
+const apareceMenu = (toggleId, navId) => {
+      const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId)
+
+      if (toggle && nav) {
+            toggle.addEventListener('click', () => {
+                  nav.classList.toggle('show')
             });
-        }
-    };
-    // Run the show!
-    filterList.init();
-});
-
-
-
-function defer(method) {
-    if (window.jQuery)
-        method();
-    else
-        setTimeout(function () {
-            defer(method)
-        }, 50);
+      }
 }
-defer(function () {
-    (function ($) {
 
-        function doneResizing() {
-            var totalScroll = $('.logo-slider-frame').scrollLeft();
-            var itemWidth = $('.logo-slider-item').width();
-            var difference = totalScroll % itemWidth;
-            if (difference !== 0) {
-                $('.logo-slider-frame').animate({
-                    scrollLeft: '-=' + difference
-                }, 500, function () {
-                    checkArrows();
-                });
-            }
-        }
+apareceMenu('nav_mobile', 'nav_menu')
 
-        function checkArrows() {
-            var totalWidth = $('#logo-slider .logo-slider-item').length * $('.logo-slider-item').width();
-            var frameWidth = $('.logo-slider-frame').width();
-            var itemWidth = $('.logo-slider-item').width();
-            var totalScroll = $('.logo-slider-frame').scrollLeft();
+const navLink = document.querySelectorAll('.nav_link')
+navLink.forEach(n => n.classList.remove('ativo'))
 
-            // if ( ((totalWidth - frameWidth) - totalScroll) < itemWidth ) {
-            //   $(".next").css("visibility", "hidden");
-            // }
-            // else {
-            //   $(".next").css("visibility", "visible");
-            // }
-            // if ( totalScroll < itemWidth ) {
-            //   $(".prev").css("visibility", "hidden");
-            // }
-            // else {
-            //   $(".prev").css("visibility", "visible");
-            // }
-        }
+function linkAction() {
+      navLink.forEach(n => n.classList.remove('ativo'))
+      this.classList.add('ativo')
 
-        $('.arrow').on('click', function () {
-            var $this = $(this),
-                width = $('.logo-slider-item').width(),
-                speed = 500;
-            if ($this.hasClass('prev')) {
-                $('.logo-slider-frame').animate({
-                    scrollLeft: '-=' + width
-                }, speed, function () {
-                    checkArrows();
-                });
-            } else if ($this.hasClass('next')) {
-                $('.logo-slider-frame').animate({
-                    scrollLeft: '+=' + width
-                }, speed, function () {
-                    checkArrows();
-                });
-            }
-        });
+      const navMenu = document.getElementById('nav_menu')
+      navMenu.classList.remove('show')
+}
 
-        $(window).on("load resize", function () {
-            checkArrows();
-            $('#logo-slider .logo-slider-item').each(function (i) {
-                var $this = $(this),
-                    left = $this.width() * i;
-                $this.css({
-                    left: left
-                })
-            });
-        });
-
-        var resizeId;
-        $(window).resize(function () {
-            clearTimeout(resizeId);
-            resizeId = setTimeout(doneResizing, 500);
-        });
-
-    })(jQuery);
-});
+navLink.forEach(n => n.addEventListener('click', linkAction))
