e0bc70de2691d79da8852cd76c4a9f3b5040bdf4
gui1535 <guilherme.araujo1535@gmail.com>
2022-06-01 15:27:39 -0300

    Imagens

---
+++ js/notificacao.js
@@ -0,0 +1,6 @@
+// Notificação Curriculo
+setTimeout(function () {
+    document.getElementById('mensagem-sucesso-curriculo').onclick = function () {
+        swal('Boa!', 'Muito obrigado por fazer o download do meu curriculo!', 'success')
+    };
+}, 190);
